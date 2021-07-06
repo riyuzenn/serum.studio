bf841cc05db3797ca3aed35cf0cb141b5136280b
Zenqi <zenaiwellplayed@gmail.com>
2021-07-06 15:37:24 +0800

    update files

--- pages/anglo.tsx
+++ pages/anglo.tsx
@@ -1,7 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Terminal from "../components/terminal";
 
+var term_data = [
+  { index: 0, text: "# Anglo, a modern lightweight web framework.", },
+  { index: 1, text: "# Built with pure Python 3.", },
+  { index: 2, text: "# Lightweight and fast.", },
+  { index: 3, text: "# Decorator based, easy to install.", },
+  { index: 4, text: "pip install anglo.", },
+]
   
 
 export default function Home() {
@@ -17,7 +25,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-      
+        <Terminal data={term_data} success_text={'Anglo is successfully installed.'} ></Terminal>
         <p className={styles.description}>
           Get started by editing{' '}
           
