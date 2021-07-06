c325d8db1cdda4f1b6f05d56d20645d432c4270b
Zenqi <zenaiwellplayed@gmail.com>
2021-07-06 16:24:40 +0800

    update files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useCallback, useEffect, useState } from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { GeistProvider, CssBaseline } from '@geist-ui/react';
+import { PrefersContext, themes, ThemeType } from '../libs/use-prefers';
+import Menu from '../components/header/menu';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const SerumApp = ({ Component, pageProps }: AppProps) => {
+  const [themeType, setThemeType] = useState<ThemeType>('light');
+
+  useEffect(() => {
+    document.documentElement.removeAttribute('style');
+    document.body.removeAttribute('style');
+
+    const theme = window.localStorage.getItem('theme') as ThemeType;
+    if (themes.includes(theme)) setThemeType(theme);
+  }, []);
+
+  const switchTheme = useCallback((theme: ThemeType) => {
+    setThemeType(theme);
+    if (typeof window !== 'undefined' && window.localStorage) window.localStorage.setItem('theme', theme);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <title>React Dashboard Design</title>
+        <meta name="og:title" content="Serum Studio" />
+        <meta name="og:description" content="..." />
+        <meta name="description" content="..." />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GeistProvider themeType={themeType}>
+        <CssBaseline />
+        <PrefersContext.Provider value={{ themeType, switchTheme }}>
+          <Menu />
+          <Component {...pageProps} />
+        </PrefersContext.Provider>
+      </GeistProvider>
+    </>
+  );
+};
+
+export default SerumApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from '@geist-ui/react'
+
+class MyDocument extends Document {
+  static async getInitialProps (ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      )
+    }
+  }
+
+  render() { 
+    return (
+        <Html lang="en-US">
+          <Head>
+            
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      );
+   }
+}
+
+
+export default MyDocument;
