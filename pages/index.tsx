25a843965bc44448a350d2163f6115110b4d596f
Zenqi <66673988+znqi@users.noreply.github.com>
2021-07-08 01:24:14 +0800

    Change `/project` -> `/projects`

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const HomePage = () => {
             </p>
           </div>
           <div className={style.hero__btn__row}>
-            <Button icon={ <Icons.Layers /> } className={style.hero__btn} type="secondary" onClick={() => {window.open('/project')}}>
+            <Button icon={ <Icons.Layers /> } className={style.hero__btn} type="secondary" onClick={() => {window.open('/projects')}}>
               Projects
             </Button>
             <Button icon={<Icons.Twitter />} type="secondary" ghost className={style.hero__btn} onClick={() => { window.open('https://twitter.com/serumstudio', '_blank') }}>
